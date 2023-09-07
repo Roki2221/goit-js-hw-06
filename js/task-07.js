1ea2fc7379b806c82b0a1e354c52d19a8fac8e78
Roki2221 <100676027+Roki2221@users.noreply.github.com>
2023-09-07 14:03:13 +0300

    FIX

--- js/task-07.js
+++ js/task-07.js
@@ -3,7 +3,6 @@ const text = document.querySelector("#text");
 
 const changeFontSize = () => {
   text.style.fontSize = `${rangeInput.value}px`;
-  console.log(rangeInput.value);
 };
 
 rangeInput.addEventListener("input", changeFontSize);
